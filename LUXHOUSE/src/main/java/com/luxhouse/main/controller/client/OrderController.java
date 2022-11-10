3303e3074a3b44c2219ee80f6315194bde556967
huyletran007 <huyletran007@gmail.com>
2022-11-11 00:33:04 +0700

    update

--- LUXHOUSE/src/main/java/com/luxhouse/main/controller/client/OrderController.java
+++ LUXHOUSE/src/main/java/com/luxhouse/main/controller/client/OrderController.java
@@ -1,5 +1,26 @@
 package com.luxhouse.main.controller.client;
 
-public class OrderController {
+import java.util.Arrays;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class OrderController extends com.luxhouse.main.controller.Controller {
+    @GetMapping("/OrderDetail")
+    public String layout(Model model) {
+        dataLayoutMaster.setView("client/order/order-detail");
+        dataLayoutMaster.setJsList(Arrays.asList(""));
+
+        objsDataLayout.put("key", "value");
+
+        dataLayoutMaster.setObjs(objsDataLayout);
+
+        model.addAttribute(__CONTENT__, dataLayoutMaster);
+
+        return clientLayout;
+    }
 }
