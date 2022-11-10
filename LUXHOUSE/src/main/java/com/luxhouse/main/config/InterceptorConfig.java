37f160796921316fb2f6103e001c64e76ba03b8b
lqtoan113 <105216751+lqtoan113@users.noreply.github.com>
2022-11-10 23:57:29 +0700

    Toan
    Show Cate

---
+++ LUXHOUSE/src/main/java/com/luxhouse/main/config/InterceptorConfig.java
@@ -0,0 +1,20 @@
+package com.luxhouse.main.config;
+
+import com.luxhouse.main.interceptor.GlobalInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Autowired
+    GlobalInterceptor globalInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(globalInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/rest/**","/admin/**","/assets/**");
+    }
+}
---
+++ LUXHOUSE/src/main/java/com/luxhouse/main/interceptor/GlobalInterceptor.java
@@ -0,0 +1,28 @@
+package com.luxhouse.main.interceptor;
+
+import com.luxhouse.main.repository.CategoryRepository;
+import com.luxhouse.main.service.CategoryProductService;
+import com.luxhouse.main.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class GlobalInterceptor implements HandlerInterceptor{
+
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    CategoryProductService categoryProductService;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+        request.setAttribute("cates",categoryService.findAll());
+        request.setAttribute("cate_pro",categoryProductService.findAll());
+    }
+}
